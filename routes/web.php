f8dc0cf68f917ab8086c1e3694054adeccea639f
WindowsP139 <WindowsP139@Win10.com>
2019-12-25 22:22:21 +0530

    Laravel:5.7Scratch:Episode:25:Middleware

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
 //laravel container is the application and we can access as app()
-app()->bind('example',function(){
+//app()->bind('example',function(){
     //we can read any of the conflict file using filename and key
     $foo = config('services.foo');
     return new\App\Example($foo);
-});
-Route::get('/',function(){
+//});
+//Route::get('/',function(){
     $example = resolve('example');
 
     ddd($example);
-});
+//});
+Auth::routes();
+Route::get('/home','HomeController@index')->name('home');
